6393d1cd06e2da3db737c267d7496004e82699a3
Jonathan Lewis <jonathan.lewis@welch.edu>
2023-07-05 15:37:26 -0500

    Finally a decent solution... Not the best  but it works! lol

--- palidrome_number.cpp
+++ palidrome_number.cpp
@@ -6,51 +6,50 @@ using namespace std;
 bool isPalindrome(int x);
 
 int main(){
-    int  test = 1000021;
+    int  test = 100002499;
     bool  flag = isPalindrome(test);
     cout << flag << endl;
-return 0;
+    return 0;
 }
 
 bool isPalindrome(int x) {
-        if(x < 0){
-            return false;
-        }
-        vector<int> pali;
-        double number_to_divide_by_tens = x;
-        double next_number_to_use = x;
-        double number_to_subtract = 0;
-        double number_to_push = 0;
-        int tens[7] = {10000000, 1000000, 100000, 10000, 1000, 100, 10};
-    int digits = 0;
+    if(x < 0){
+        return false;
+    }
+    vector<int> pali;
     int number = x;
+    int digits = 0;
+    int current_power;
+    int current_number = x;
+    int current_number_copy = 0;
+    int subtract_by = 0;
+    int floor_number_to_push = 0;
+    
     while(number != 0){
         number = number / 10;
         digits += 1;
     }
-    int iterate = digits;
-    cout << digits << endl;
-        for(int i = 0; i < iterate; i++){
-            next_number_to_use = number_to_divide_by_tens /  pow(10, (digits-1));
-            cout << next_number_to_use << endl;
-            //if(next_number_to_use >= 1){
-                number_to_push = floor(next_number_to_use);
-                pali.push_back(number_to_push);
-                number_to_subtract = number_to_push * tens[i];
-                number_to_divide_by_tens -= number_to_subtract;
-            digits -= 1;
-            //}
-        }
-    pali.push_back(number_to_divide_by_tens);
-
     
-    
-        int length = pali.size();
-        cout << length << endl;
-        for(int y = 0; y < (floor(length/2)); y++){
-            if(pali[y] != pali[(length-y)-1]){
-                return false;
-            }
-        }
-        return true;
+    int power_digits = digits - 1;
+    for(int i = 0; i < digits; i++){
+        current_power = pow(10, power_digits);
+        current_number_copy = current_number / current_power;
+        floor_number_to_push = floor(current_number_copy);
+        pali.push_back(floor_number_to_push);
+        subtract_by = current_power * floor_number_to_push;
+        current_number -= subtract_by;
+        
+        power_digits -= 1;
     }
+    for(int x = 0; x < digits; x ++){
+        cout << pali[x] << endl;
+    }
+    
+    int length = pali.size();
+    for(int y = 0; y < (floor(length/2)); y++){
+        if(pali[y] != pali[(length-y)-1]){
+            return false;
+        }
+    }
+    return true;
+}
